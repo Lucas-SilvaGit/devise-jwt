8149ad652b764790b0c043a570145f572ee9a91d
Jose Lucas <lucasnba2324@gmail.com>
2023-10-31 20:11:15 -0300

    creating current user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_name: {
+  get '/current_user', to: 'current_user#index'
+  
+  devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
